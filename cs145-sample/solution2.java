a3e752eaf7ca3094f2cd9d9c98f1ea89538b7d5a
Suraj Subrahmanyan <ssubrahmanyan11@gmail.com>
2025-06-30 18:46:26 -0400

    Revise to solution2 to become more effecient

--- cs145-sample/solution2.java
+++ cs145-sample/solution2.java
@@ -17,28 +17,10 @@ public class solution2{
     }
 
     public static int max3(int x1, int x2, int x3){
-        if (x1>x2 && x1>x3){
+        if (x1>=x2 && x1>=x3){
             return x1;
-        } else if(x2>x1 && x2>x3){
+        } else if(x2>=x1 && x2>=x3){
             return x2;
-        } else if(x1==x2){
-            if (x1>x3){
-                return x1;
-            } else {
-                return x3;
-            }
-        } else if(x2==x3){
-            if (x2>x1){
-                return x2;
-            } else {
-                return x1;
-            }
-        } else if(x1==x3){
-            if (x1>x2) {
-                return x1;
-            } else {
-                return x2;
-            }
         } else {
                 return x3;
             }
