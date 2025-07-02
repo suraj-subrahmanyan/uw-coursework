241469573beff6b5fc1434b433ebab42c8842621
Suraj Subrahmanyan <ssubrahmanyan11@gmail.com>
2025-07-02 00:03:17 -0400

    Revise solution 4 to be recursive

--- cs145-sample/solution4.java
+++ cs145-sample/solution4.java
@@ -2,12 +2,15 @@ public class solution4{
     public static boolean treesimilair(t1, t2){
         if (t1 == null && t2 == null){
             return true;
-        } else if ((t1 != null && t2 != null) && t1.right == t2.right) {
-            return true;            
-        } else if ((t1 != null && t2 != null) && t1.left == t2.left) {
-            return true;            
-        } else {
+        }
+
+        if (t1 == null && t1 != null){
             return false;
+        }
+        
+        if (t1 != null && t2 != null){
+            boolean left=treesimilair(t1.left, t1.right)
+            boolean right=treesimilair(t1.left, t1.right)
         }
     }
 }
